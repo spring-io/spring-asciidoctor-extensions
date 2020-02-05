91401a99ff2b12772d7ebf89f7e9bdb62b6f5528
Andy Wilkinson <awilkinson@pivotal.io>
2020-02-05 12:16:51 +0000

    Replace publishing to custom Maven local with distribution repository
    Closes gh-35

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -5,5 +5,5 @@ source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
 
 pushd git-repo > /dev/null
-./gradlew --no-daemon build publishToMavenLocal -Dmaven.repo.local=${repository}
+./gradlew --no-daemon build publish -PdistributionRepository=${repository}
 popd > /dev/null

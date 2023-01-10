72c350cf97cc029f77957feb612c297c2f9a4df2
Andy Wilkinson <wilkinsona@vmware.com>
2023-01-10 10:38:57 +0000

    Upgrade CI Image to Java 8u352+8

--- ci/images/setup.sh
+++ ci/images/setup.sh
@@ -18,7 +18,7 @@ curl https://raw.githubusercontent.com/spring-io/concourse-java-scripts/v0.0.3/c
 ###########################################################
 # JAVA
 ###########################################################
-JDK_URL='https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u265-b01/OpenJDK8U-jdk_x64_linux_hotspot_8u265b01.tar.gz'
+JDK_URL='https://github.com/bell-sw/Liberica/releases/download/8u352+8/bellsoft-jdk8u352+8-linux-amd64.tar.gz'
 
 mkdir -p /opt/openjdk
 cd /opt/openjdk
